54812ecfcff726cc0591bfd10ab8191de77da472
hutaille <hugo.taillefumier@uca.fr>
2024-11-14 13:59:44 +0100

    responsive

--- assets/pages/StudentView.vue
+++ assets/pages/StudentView.vue
@@ -1,11 +1,10 @@
 <template>
-  <div v-if="this.docs !== null && Object.keys(docs.data).length > 0" class="container">
+  <div v-if="this.docs !== null && Object.keys(docs.data).length > 0" class="container-lg">
 
     <div class="mt-2 mb-2 row">
       <h4>Documents de scolarité</h4>
       <h5>{{ this.docs.student.nom }} {{ this.docs.student.prenom }} ({{ this.docs.student.numero }})</h5>
     </div>
-
 
     <div class="accordion" id="accordionPanelsStayOpenExample">
       <div v-for="(year, index) in Object.keys(docs.data).reverse()" class="accordion-item mt-2">
@@ -21,7 +20,7 @@
              :aria-labelledby="'panelsStayOpen-heading' + year">
           <div class="accordion-body">
             <div class="row">
-              <div class="col-6">
+              <div :class="'col-12 col-lg-6 ' + (docs.data[year].rn === undefined ? 'collapse' : '')">
                 <h6><strong>Relevés de notes</strong></h6>
                 <ul>
                   <li v-for="rn in docs.data[year].rn"><a
@@ -29,7 +28,7 @@
                       target="_blank">{{ rn.name }}</a></li>
                 </ul>
               </div>
-              <div class="col-6">
+              <div :class="'col-12 col-lg-6 ' + (docs.data[year].attest === undefined ? 'collapse' : '')">
                 <h6><strong>Attestations de réussite</strong></h6>
                 <ul>
                   <li v-for="attest in docs.data[year].attest"><a
@@ -43,11 +42,17 @@
       </div>
     </div>
 
-
   </div>
 
   <div v-else class="container">
-    <div class="alert alert-warning m-5"> Aucun document</div>
+
+    <div class="mt-2 mb-2 row">
+      <h4>Documents de scolarité</h4>
+      <h5 v-if="this.docs !== null">{{ this.docs.student.nom }} {{ this.docs.student.prenom }}
+        ({{ this.docs.student.numero }})</h5>
+    </div>
+
+    <div class="alert alert-warning m-5 text-center">Aucun document vous concernant n'a été déposé par votre service scolarité</div>
   </div>
 </template>
 
@@ -68,17 +73,9 @@ export default {
       return WebService
     },
     fetchDocs() {
-
-      // let num = 0;
-
-      // if (user.isEtudiant())
-      //   num = user.numero;
-      // else
-      //   num = this.$route.params.num;
-
       WebService.getStudentDocs(this.num).then(response => {
         this.docs = response.data;
-        //console.log(this.docs);
+        // console.log(this.docs);
       }).catch(error => {
         console.log("Error fetching docs");
       })
